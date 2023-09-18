7d2042d6ec5767343e541345932c8a810cc643a8
fidan.ahmadova <FidanAkhmedova>
2023-09-18 17:43:44 +0400

    initial commit

--- src/main/java/com/example/order/OrderService.java
+++ src/main/java/com/example/order/OrderService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderService {
-
+//
     private OrderNameService orderNameSequence;
     public Order createNewOrder(){
         Order order = new Order();
